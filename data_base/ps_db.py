2f4df25d1412bdf592fbbaabaa74a266889c27e1
FlashDrag <flashdrag@gmail.com>
2022-07-23 10:10:39 +0100

    fixed: usage counter decorator; pep8

--- data_base/ps_db.py
+++ data_base/ps_db.py
@@ -6,7 +6,7 @@ import logging
 import logging.config
 
 logging.config.fileConfig('logging/logging.conf',
-                        disable_existing_loggers=False)
+                          disable_existing_loggers=False)
 log = logging.getLogger("db")
 
 
@@ -20,19 +20,20 @@ class BotDB:
             conn = ps.connect((DB_URL), sslmode='require')
         except TypeError or ps.ProgrammingError:
             log.error('Failed connection to the database or incorrect URL')
-            return  
+            return
         cursor = conn.cursor()
         self.conn, self.cursor = conn, cursor
 
         cursor.execute("CREATE TABLE IF NOT EXISTS users(user_id BIGINT PRIMARY KEY, username TEXT, first_name TEXT, "
                        "last_name TEXT, join_date TIMESTAMP, bug_text TEXT)")
 
-        cursor.execute("CREATE TABLE IF NOT EXISTS game_stat(user_id BIGINT PRIMARY KEY, max_num INTEGER, win_count INTEGER, "
-                       "win_count_for_stat INTEGER, max_num_for_stat INTEGER, game_over_count INTEGER, last_win_date TIMESTAMP, last_over_date TIMESTAMP)")
+        cursor.execute('''CREATE TABLE IF NOT EXISTS game_stat(user_id BIGINT PRIMARY KEY, max_num INTEGER,
+                        win_count INTEGER, win_count_for_stat INTEGER, max_num_for_stat INTEGER,
+                        game_over_count INTEGER, last_win_date TIMESTAMP, last_over_date TIMESTAMP)''')
 
         cursor.execute("CREATE TABLE IF NOT EXISTS usage(user_id BIGINT PRIMARY KEY, welcome_count INTEGER, "
-                        "start_game_count INTEGER, info_count INTEGER, help_count INTEGER, pidkazka_count INTEGER, "
-                        "progress_count INTEGER, rating_count INTEGER, reset_count INTEGER, cancel_count INTEGER)")
+                       "start_game_count INTEGER, info_count INTEGER, help_count INTEGER, pidkazka_count INTEGER, "
+                       "progress_count INTEGER, rating_count INTEGER, reset_count INTEGER, cancel_count INTEGER)")
 
         conn.commit()
 
@@ -60,7 +61,7 @@ class BotDB:
                                 (user_id, 0, 0, 0, 0, 0, 0, 0, 0, 0))
             self.cursor.execute("INSERT INTO game_stat (user_id, max_num, win_count, win_count_for_stat, "
                                 "max_num_for_stat, game_over_count) VALUES (%s, %s, %s, %s, %s, %s) ",
-                                (user_id, 10, 0, 0, 0, 0))                 
+                                (user_id, 10, 0, 0, 0, 0))
         self.conn.commit()
 
     def add_user(self, message):
@@ -72,13 +73,13 @@ class BotDB:
             last_name = message.from_user.last_name
             username = message.from_user.username
             full_name = message.from_user.full_name
-            self.cursor.execute("INSERT INTO users (user_id, username, first_name, last_name , join_date) "
-                            "VALUES (%s,%s,%s,%s,%s)", (user_id, username, first_name, last_name , datetime.datetime.now()))
+            self.cursor.execute('''INSERT INTO users (user_id, username, first_name, last_name , join_date)
+                                VALUES (%s,%s,%s,%s,%s)''',
+                                (user_id, username, first_name, last_name, datetime.datetime.now()))
             self.add_usage(user_id)
             log.info(f"The user: '{user_id} - {first_name}' has been added to DB")
         self.conn.commit()
 # adding new user-----------------
-       
 
 # update_game_stat-----------------
     def update_win_count(self, user_id):
@@ -88,14 +89,15 @@ class BotDB:
 
     def update_win_count_for_stat(self, user_id):
         """Обновляем данные в базе"""
-        self.cursor.execute("UPDATE game_stat SET win_count_for_stat = win_count_for_stat + 1 WHERE user_id = %s", (user_id,))
-        return self.conn.commit()        
+        self.cursor.execute("UPDATE game_stat SET win_count_for_stat = win_count_for_stat + 1 WHERE user_id = %s",
+                            (user_id,))
+        return self.conn.commit()
 
     def update_max_num(self, user_id):
         """Обновляем данные в базе"""
         self.cursor.execute("UPDATE game_stat SET max_num = max_num * 2 WHERE user_id = %s", (user_id,))
         return self.conn.commit()
-    
+
     def update_max_num_for_stat(self, user_id, max_num):
         """Обновляем данные в базе"""
         self.cursor.execute("UPDATE game_stat SET max_num_for_stat = %s WHERE user_id = %s", (max_num, user_id,))
@@ -115,29 +117,28 @@ class BotDB:
         return self.conn.commit()
 # update_game_stat-----------------
 
-
 # update_usage---------------------
     # decorator
-    def update_usage_decorator(self, func):
+    def usage_counter(self, func):
         '''
-        Функция считает использования юзером декорироваемого хендлера и сохраняет значения в БД 
+        Функция считает использования юзером декорироваемого хендлера и сохраняет значения в БД
         '''
         def wraper(*args, **kwargs):
             """Обновляем данные в базе"""
             res = func(*args, **kwargs)
             user_id = args[0].from_user.id
-            counter_name = func.__name__.replace('process_', '') + '_count'
-            self.cursor.execute("UPDATE usage SET %s += 1 WHERE user_id = %s", (counter_name, user_id))
+            counter_name = func.__name__.replace('process_', '').replace('_command', '') + '_count'
+            self.cursor.execute(f"UPDATE usage SET {counter_name} = {counter_name} + 1 WHERE user_id = ?", (user_id,))
             self.conn.commit()
             return res
         return wraper
-
 
     def update_welcome_count(self, user_id):
         """Обновляем данные в базе"""
         self.cursor.execute("UPDATE usage SET welcome_count = welcome_count + 1 WHERE user_id = %s", (user_id,))
         return self.conn.commit()
 
+    # not decorated yet
     def update_start_game_count(self, user_id):
         """Обновляем данные в базе"""
         self.cursor.execute("UPDATE usage SET start_game_count = start_game_count + 1 WHERE user_id = %s", (user_id,))
@@ -146,11 +147,6 @@ class BotDB:
     def update_info_count(self, user_id):
         """Обновляем данные в базе"""
         self.cursor.execute("UPDATE usage SET info_count = info_count + 1 WHERE user_id = %s", (user_id,))
-        return self.conn.commit()
-
-    def update_help_count(self, user_id):
-        """Обновляем данные в базе"""
-        self.cursor.execute("UPDATE usage SET help_count = help_count + 1 WHERE user_id = %s", (user_id,))
         return self.conn.commit()
 
     def update_help_count(self, user_id):
@@ -168,11 +164,12 @@ class BotDB:
         self.cursor.execute("UPDATE usage SET rating_count = rating_count + 1 WHERE user_id = %s", (user_id,))
         return self.conn.commit()
 
+    # not decorated yet
     def update_pidkazka_count(self, user_id):
         """Обновляем данные в базе"""
         self.cursor.execute("UPDATE usage SET pidkazka_count = pidkazka_count + 1 WHERE user_id = %s", (user_id,))
         return self.conn.commit()
-    
+
     def update_reset_count(self, user_id):
         """Обновляем данные в базе"""
         self.cursor.execute("UPDATE usage SET reset_coun = reset_count + 1 WHERE user_id = %s", (user_id,))
@@ -184,7 +181,6 @@ class BotDB:
         return self.conn.commit()
 # update_usage---------------------
 
-
 # reset---------------------
     def reset_values(self, user_id):
         """Сброс данных в базе"""
@@ -192,7 +188,6 @@ class BotDB:
         self.cursor.execute("UPDATE game_stat SET win_count = 0 WHERE user_id = %s", (user_id,))
         return self.conn.commit()
 # reset---------------------
-
 
 # get data---------------------
     def get_user_name(self, user_id: int):
@@ -209,7 +204,7 @@ class BotDB:
         """Достаем max_num с базы по user_id"""
         self.cursor.execute("SELECT max_num FROM game_stat WHERE user_id = %s", (user_id,))
         return self.cursor.fetchone()[0]
-    
+
     def get_max_num_for_stat(self, user_id):
         """Достаем max_num с базы по user_id"""
         self.cursor.execute("SELECT max_num_for_stat FROM game_stat WHERE user_id = %s", (user_id,))
@@ -218,23 +213,25 @@ class BotDB:
     def get_win_count(self, user_id):
         """Достаем user_win с базы по user_id"""
         self.cursor.execute("SELECT win_count FROM game_stat WHERE user_id = %s", (user_id,))
-        return self.cursor.fetchone()[0]   
+        return self.cursor.fetchone()[0]
 
     def get_rating(self, message):
         """Достаем значения c разных таблиц с БД и формируем 2 списка с кортежами(users, win_count).
         Через цикл формируем словарь: first_name получаем с users, win_count берем с game_stat.
-        Выводим в таблице с сортировкой по к-стве выиграшей. 
-        Чтобы таблица не встала криво в телеге выводим через: f'<pre>{db.get_rating()}</pre>', parse_mode=types.ParseMode.HTML
+        Выводим в таблице с сортировкой по к-стве выиграшей.
+        Чтобы таблица не встала криво в телеге выводим через:
+        f'<pre>{db.get_rating()}</pre>', parse_mode=types.ParseMode.HTML
         """
         table = PrettyTable()
         table.field_names = ["--Ігрок--", "--Рівень--"]
-        
+
         self.cursor.execute("SELECT user_id, first_name FROM users")
         users = self.cursor.fetchall()
         self.cursor.execute("SELECT user_id, win_count FROM game_stat")
         win_count = self.cursor.fetchall()
         rait_dict = {}
-        # в цикле сравниваем `id`, если одинаковые - в словарь добавляем ключ как `first_name` с `users`, `value` как `win_count` с `game_stat`
+        # в цикле сравниваем `id`, если одинаковые - в словарь добавляем ключ как `first_name` с `users`,
+        # `value` как `win_count` с `game_stat`
         for i in users:
             for j in win_count:
                 if i[0] == j[0]:
@@ -247,25 +244,22 @@ class BotDB:
                 continue
             else:
                 table.add_row([i, rait_dict[i]])
-        table.sortby = "--Рівень--"     # сортировка таблицы по заголовку колонки 
+        table.sortby = "--Рівень--"     # сортировка таблицы по заголовку колонки
         table.reversesort = True
         return(table)
 
         self.cursor.execute("SELECT first_name, user_win FROM users ORDER BY user_win")
         users = from_db_cursor(self.cursor)
         return(users)
-        # или вывод данных с БД прямо в таблицу 
+        # или вывод данных с БД прямо в таблицу
         # нужен импорт:   from prettytable import from_db_cursor
         # https://zetcode.com/python/prettytable/  - инфа по prettytable
-
 
     def get_stat(self):
         '''Получаем с timestamp с базы дату в указаном формате функцией to_char()'''
         self.cursor.execute("SELECT first_name, TO_CHAR(DATE, 'DD/MM/YY HH24:MI') FROM users")
         dates = from_db_cursor(self.cursor)
         return(dates)
- 
-
 
     def set_bug_message(self, message):
         """Записываем bug_message от юзера с команды /bug в базу по user_id даного юзера"""
@@ -274,19 +268,16 @@ class BotDB:
         self.cursor.execute('UPDATE users SET bug_text = %s WHERE user_id = %s', (bug_text, user_id,))
         return self.conn.commit()
 
-    
     def get_bug_text(self, user_id_bag_message):
         """Достаем bug_text от юзера по user_id и отправляем админу"""
         user_id = user_id_bag_message
         self.cursor.execute("SELECT bug_text FROM users WHERE user_id = %s", (user_id,))
         return self.cursor.fetchone()
 
-
     # def get_stat(self):
     #     # self.cursor.execute("SELECT first_name, date :: timestamp::date from users")
     #     x = PrettyTable()
     #     x.field_names = ["ЮЗЕР", "дата"]
-        
     #     self.cursor.execute("SELECT first_name FROM users")
     #     users = self.cursor.fetchone()
     #     self.cursor.execute("SELECT date :: timestamp::date FROM users")
@@ -295,15 +286,13 @@ class BotDB:
     #     hours = self.cursor.fetchall()
     #     self.cursor.execute("SELECT date_part('minute', date::TIMESTAMP) FROM users")
     #     minutes = self.cursor.fetchall()
-        
-        
+
     #     for name in users:
     #         for date in dates:
     #             x.add_row(name, date)
 
     #     log.info(x)
         # log.info(date)
-        
 
         '''
     def get_rating(self):
@@ -319,12 +308,6 @@ class BotDB:
         return text
         '''
 
-
-
-
-
     def close_connect(self):
         self.cursor.close()
         self.conn.close()
-    
-
