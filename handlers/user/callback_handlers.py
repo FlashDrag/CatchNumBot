96cf56aa8468a64492f148c40db2fe87afd1db74
FlashDrag <flashdrag@gmail.com>
2022-07-24 10:49:56 +0100

    pep8

--- handlers/user/callback_handlers.py
+++ handlers/user/callback_handlers.py
@@ -6,18 +6,17 @@ from aiogram.dispatcher import FSMContext
 from keyboards.user_markup.keyboard_markup import sos_menu_markup, main_start_menu_markup
 
 
-
-@dp.callback_query_handler(text='niz') 
+@dp.callback_query_handler(text='niz')
 async def help_inline_markup_callback_handler(query: CallbackQuery, state: FSMContext):
     user_id = query.from_user.id
-    db.update_pidkazka_count(user_id)  #счетчик    
+    db.update_pidkazka_count(user_id)  # счетчик
     answer_data = query.data
     await query.answer("Кожен раз ділимо на 2 діапазон, що залишився", show_alert=True)
     # always answer callback queries, even if you have nothing to say
     await bot.send_message(query.from_user.id, "Зрозуміло? Круто! Тисни старт, щоб почати\n\n "
-                                                "Досі у ваукумі? ➡️SOS⬅️ -  для зв'язку з адміном.", reply_markup=sos_menu_markup())
+                                               "Досі у ваукумі? ➡️SOS⬅️ -  для зв'язку з адміном.",
+                                               reply_markup=sos_menu_markup())
     await state.reset_state(with_data=False)
-
 
 
 # Use multiple registrators. Handler will execute when one of the filters is OK
@@ -25,7 +24,7 @@ async def help_inline_markup_callback_handler(query: CallbackQuery, state: FSMCo
 @dp.callback_query_handler(text='yes')  # if cb.user_data == 'yes'
 async def inline_kb_answer_callback_handler(query: CallbackQuery, state: FSMContext):
     answer_data = query.data
-     # always answer callback queries, even if you have nothing to say
+    # always answer callback queries, even if you have nothing to say
     if answer_data == 'yes':
         await query.message.delete()
         await process_start_query(query, state)
@@ -36,7 +35,3 @@ async def inline_kb_answer_callback_handler(query: CallbackQuery, state: FSMCont
         await query.answer(f"Покасики 😜", show_alert=True)
         await query.message.delete()
         await state.reset_state(with_data=False)
-
-
-
-        
